072fc87e5b3fa8003590929fcdb5a2a6b2f34d14
Bruce0203 <BruceFreedy@gmail.com>
2022-02-07 14:05:11 +0900

    pos print to screen overlay

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,20 +1,24 @@
 import com.soywiz.korge.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
+import com.soywiz.korim.font.TtfFont
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(bgcolor = Colors["#2b2b2b"]) {
 	val image = image(resourcesVfs["player.png"].readBitmap()) {
 		anchor(.5, .5)
 		scale(.8)
 		position(256, 256)
 	}
 	val input = views.input
+	val font = TtfFont(resourcesVfs["NanumGothic.ttf"].readAll())
+	val text = text(image.pos.toString(), textSize = 32.0, font = font)
 	addUpdater {
 		val xy: Point = input.mouse
 		val buttons: Int = input.mouseButtons // flags with the pressed buttons
 		image.pos = xy
+		text.text = image.pos.toString()
 	}
 }
